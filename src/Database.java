76c73632b29590d472311f2cf576cbfa74a75901
Tulio-Santana-Ramos <santana.tulior@gmail.com>
2021-07-26 19:18:46 -0300

    Estado Pronto e Comentado

--- src/Database.java
+++ src/Database.java
@@ -1,18 +1,24 @@
 import java.util.Random;
 
 /**
- * Classe utilizada para originar os dados "ficticios" a serem utilizados no programa principal
- * a idéia desta classe é simular um servidor que forneceria as informações atualizadas ao software
+ * Classe utilizada para originar os dados "ficticios" a serem utilizados no programa principal,
+ * a idéia desta classe é simular um servidor que forneceria as informações atualizadas ao software.
+ * 
+ * A idéia para a geração dos valores é determinar um valor inicial para cada opção (infectados, mortos, etc...)
+ * e, além disso, gerar uma taxa máxima de crescimento para cada estado, e então a partir dai gerar os valores 
+ * se baseando no valor anterior acrescido por uma taxa de crescimento randômica e limitada pela taxa de crescimento
+ * máximo.
  */
 
 public class Database {
 	
-	public int getDias() {
+	public int getDias() { // retorna o número de dias passados que os dados serão observados
 		return 50;
 	}
 	
 	Random rd = new Random();
 	
+	// Vetores com valores auxiliares para a geração dos dados fictícios a serem usados
 	int[] infectados_iniciais = {159383 , 100886 , 162777 , 106915 , 117793 , 108335 , 155386 , 130492 , 86649 , 111421 , 72362 , 160027 , 225000 , 138690 , 90059 , 167763 , 83926 , 150540 , 325500 , 153426 , 159172 , 125736 , 75211 , 165368 , 450000 , 72567 , 126429};
 	int[] mortos_iniciais = {13383 , 12886 , 20777 , 26915 , 17793 , 15335 , 15386 , 10492 , 18649 , 11421 , 12362 , 12027 , 15000 , 18690 , 10059 , 18763 , 17926 , 10540 , 25000 , 13426 , 19172 , 25736 , 15211 , 25368 , 50000 , 12567 , 16429};
 	int[] recuperados_iniciais = {139383 , 110886 , 122777 , 126915 , 147793 , 148335 , 145386 , 140492 , 166649 , 101421 , 92362 , 120027 , 180000 , 88690 , 100059 , 87763 , 103926 , 140540 , 200000 , 113426 , 149172 , 155736 , 165211 , 125368 , 300000 , 122567 , 116429};
@@ -21,7 +27,7 @@ public class Database {
 	
 	double[] rgmax = {0.0891698 , 0.0100236 , 0.0824476 , 0.0772022 , 0.099731 , 0.0104564 , 0.0108233 , 0.0109778 , 0.010316 , 0.0104008 , 0.0109388 , 0.0101072 ,  0.13 , 0.0103396 , 0.0113194 , 0.0103129 , 0.0102839 , 0.0101686 ,  0.01 , 0.0102478 , 0.0100057 , 0.0102882 , 0.0100829 , 0.0108115 ,  0.012 , 0.010931 , 0.0109757};
 
-	
+	// retorna um vetor com valores fictícios do número de infectados (acumulado) de um determinado estado para uma quantidade respectiva de dias
 	public int[] getInfectados(int id, int dias){
 		int[] dados = new int[dias];
 		
@@ -38,6 +44,7 @@ public class Database {
 		return dados;
 	}
 	
+	// retorna um vetor com valores fictícios do número de mortos (acumulado) de um determinado estado para uma quantidade respectiva de dias
 	public int[] getMortos(int id, int dias){
 		int[] dados = new int[dias];
 		
@@ -54,6 +61,7 @@ public class Database {
 		return dados;
 	}
 	
+	// retorna um vetor com valores fictícios do número de recuperados (acumulado) de um determinado estado para uma quantidade respectiva de dias
 	public int[] getRecuperados(int id, int dias){
 		int[] dados = new int[dias];
 		
@@ -70,6 +78,7 @@ public class Database {
 		return dados;
 	}
 	
+	// retorna um vetor com valores fictícios do número de vacinados (acumulado) de um determinado estado para uma quantidade respectiva de dias
 	public int[] getVacinados(int id, int dias){
 		int[] dados = new int[dias];
 		
@@ -86,10 +95,11 @@ public class Database {
 		return dados;
 	}
 	
+	// retorna um vetor com valores fictícios da data de vacinação para cada respectivo grupo prioritário de um determinado estado
 	public int[] getDataVacina(int id) {
 		int[] res = new int[6];
 		int ini = data_vacina[id];
-		for(int groups = 1; groups <= 5; groups++) {
+		for(int groups = 1; groups <= 5; groups++) { // percorre os grupos prioritários
 			res[groups] = ini;
 			double randomValue = 0.1 + (0.35 - 0.1) * rd.nextDouble();
 			ini = 30 + (int)((double)ini*(1.0 + randomValue));
@@ -97,26 +107,12 @@ public class Database {
 		return res;
 	}
 	
+	// retorna um vetor de Strings com notícias fictícias sobre a pandemia em um determinado estado
 	public String[] getNews(int id) {
 		String[] ans = new String[2];
 		ans[0] = "Lote de vacinas da Coronavac, com aproximadamente " + (int)(0.4*vacinados_iniciais[id]) + " imunizantes acaba de chegar ao estado.";
 		ans[1] = "Média móvel de infectados segue a uma taxa de crescimento de " + (int)(100.0*((0.02 + rgmax[id])/2.0)) + "% ao dia."; 
 		return ans;
-	}
-	
-	public static void main(String[] args) {
-		Database d = new Database();
-		
-		/*int[] dados = d.getInfectados(24, 50);
-		
-		for(int i:dados) {
-			System.out.print(i + " ");
-		}System.out.println();
-		*/
-		String r[] = d.getNews(12);
-		for(String s:r) {
-			System.out.println(s);
-		}
 	}
 
 }
--- src/Estado.java
+++ src/Estado.java
@@ -1,3 +1,11 @@
+/**
+ * Classe utilizada para representar as características de um estado no programa principal,
+ * contendo informações advintas da classe "Database" e representando esses dados com a classe "Plot"
+ * 
+ * O estado em questão contará com um número de dias, uma id própria utilizando um enum, e os equivalentes
+ * dados que serão recebidos ao utilizarmos os métodos criados na classe "Database".
+ * Uma vez com os dados utilizaremos também os métodos da classe "Plot" para a vizualização em forma de gráficos.
+ */
 public class Estado {
 	public int dias;
 	public int id;
@@ -9,16 +17,20 @@ public class Estado {
 	public String[] noticias;
 	Database database = new Database();
 
+	//Construtor, recebendo apenas o id do estado a ser montado
+	//Utilizaremos os respectivos métodos da classe "Database" para obtermos os valores
 	public Estado(int id) {
+		this.id = id;
 		this.dias = database.getDias();
-		this.infectados = database.getInfectados(id, this.dias);
-		this.recuperados = database.getRecuperados(id, this.dias);
-		this.mortos = database.getMortos(id, this.dias);
-		this.vacinados = database.getVacinados(id, this.dias);
-		this.noticias = database.getNews(id);
-		this.diaVacinacao = database.getDataVacina(id);
+		this.infectados = database.getInfectados(this.id, this.dias);
+		this.recuperados = database.getRecuperados(this.id, this.dias);
+		this.mortos = database.getMortos(this.id, this.dias);
+		this.vacinados = database.getVacinados(this.id, this.dias);
+		this.noticias = database.getNews(this.id);
+		this.diaVacinacao = database.getDataVacina(this.id);
 	}
 
+	//Método para a plotagem dos números de infectados neste estado no decorrer de certos dias
 	public void plot_infectados() {
 		String title = "Número de Infectados";
 		String legenda = "Gráfico referente à progressão do número de infectados nos últimos" + String.valueOf(this.dias);
@@ -26,6 +38,7 @@ public class Estado {
 		Plot.plotChart(title, legenda, this.infectados);
 	}
 
+	//Método para a plotagem dos números de recuperados neste estado no decorrer de certos dias
 	public void plot_recuperados() {
 		String title = "Número de Recuperados";
 		String legenda = "Gráfico referente à progressão do número de recuperados nos últimos" + String.valueOf(this.dias);
@@ -33,6 +46,7 @@ public class Estado {
 		Plot.plotChart(title, legenda, this.recuperados);
 	}
 
+	//Método para a plotagem dos números de mortos neste estado no decorrer de certos dias
 	public void plot_mortos() {
 		String title = "Número de Mortos";
 		String legenda = "Gráfico referente à progressão do número de mortos nos últimos" + String.valueOf(this.dias);
@@ -40,6 +54,7 @@ public class Estado {
 		Plot.plotChart(title, legenda, this.mortos);
 	}
 
+	//Método para a plotagem dos números de vacinados neste estado no decorrer de certos dias
 	public void plot_vacinados() {
 		String title = "Número de Vacinados";
 		String legenda = "Gráfico referente à progressão do número de vacinados nos últimos" + String.valueOf(this.dias);
@@ -47,11 +62,13 @@ public class Estado {
 		Plot.plotChart(title, legenda, this.vacinados);
 	}
 
+	//Método para a impressão das notícias sobre a COVID-19
 	public void print_noticias() {
 		for(int i = 0; i < this.noticias.length; i++)
 			System.out.println(i + ")" + this.noticias[i] + "\n");
 	}
 
+	//Método para a impressão da data de vacinação estimada
 	public void print_data(int grupo){
 		System.out.println("Uma vez no grupo " + grupo + " vocẽ pode se vacinar dia " + this.diaVacinacao[grupo - 1]);
 	}
