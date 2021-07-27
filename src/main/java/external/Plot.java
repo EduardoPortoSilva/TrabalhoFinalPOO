a6e6f5b7db043eeda4d814fa6de84cd491068393
YannAmado <67401182+YannAmado@users.noreply.github.com>
2021-07-27 14:28:37 -0300

    Adicionados comentarios

--- src/main/java/external/Plot.java
+++ src/main/java/external/Plot.java
@@ -15,10 +15,12 @@ public class Plot {
 		height = graph_height;
 	}
 	
+	//	Define a largura do grafico
 	public static void setWidth(int graph_width) {
 		width = graph_width;
 	}
 	
+	// Define a altura do grafico
 	public static void setHeight(int graph_height) {
 		height = graph_height;
 	}
@@ -26,19 +28,30 @@ public class Plot {
 	private static void plotChart(String title, String legenda, double x_data[], double y_data[]) {
 			// Create Chart
 			XYChart chart = new XYChartBuilder().width(width).height(height).title(title).xAxisTitle("X").yAxisTitle("Y").build();
+			
+			// Coloca os pontos no grafico
 			chart.addSeries(legenda, x_data, y_data);
-	
+			
+			// Coloca a legenda para dentro do grafico no canto superior esquerdo
+			chart.getStyler().setLegendPosition(LegendPosition.InsideNW);
+			
+			// Faz um grafico linear
 			if(type.equals("Linear"))
 				chart.getStyler().setDefaultSeriesRenderStyle(XYSeriesRenderStyle.Line);
+			
+			// Faz um grafico de dispersao
 			else if(type.equals("Scatter"))
 				chart.getStyler().setDefaultSeriesRenderStyle(XYSeriesRenderStyle.Scatter);
+			
+			// Faz um grafico de area
 			else if(type.equals("Area")) 
 				chart.getStyler().setDefaultSeriesRenderStyle(XYSeriesRenderStyle.Area);
-			// Show it
+			// Printa o grafico feito
 			Thread t = new Thread(new Runnable() {
 				@Override
 				public void run() {
 					new SwingWrapper<XYChart>(chart).displayChart();
+					(chart).displayChart().setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 				}
 			});
 			t.start();	
@@ -46,7 +59,8 @@ public class Plot {
 	}
 
 	public static void plotChart(String title, String legenda, double y_data[]){
-		// Create Chart
+	
+		// Coloca os pontos X no grafico caso nao sejam fornecidos
 		int array_size = y_data.length;
 		double[] x_data = new double[array_size];  
 		for(int i = 0; i < array_size; i++) {
@@ -56,7 +70,8 @@ public class Plot {
 		
 	}
 	public static void plotChart(String title, String legenda, int x_data[], int y_data[]){
-		// Create Chart
+		
+		// Transforma os pontos em doubles  
 		int array_size = y_data.length;
 		double[] dy_data = new double[array_size];
 		double[] dx_data = new double[array_size];  
@@ -70,7 +85,8 @@ public class Plot {
 	}
 	
 	public static void plotChart(String title, String legenda, int y_data[]){
-		// Create Chart
+		
+		// Coloca os pontos X e transforma os pontos y em double
 		int array_size = y_data.length;
 		double[] dy_data = new double[array_size];
 		double[] x_data = new double[array_size];  
@@ -82,45 +98,42 @@ public class Plot {
 		
 	}
 	
+	//		GRAFICOS LINEARES
+	
 	public static void plotLinearChart(String title, String legenda, double x_data[], double y_data[]){
-		// Create Chart
 		type = "Linear";
 		Plot.plotChart(title, legenda, x_data, y_data);
 	}
 
 	public static void plotLinearChart(String title, String legenda, double y_data[]){
-		// Create Chart
 		type = "Linear";
 		Plot.plotChart(title, legenda, y_data);
 	}
 	
 	public static void plotLinearChart(String title, String legenda, int x_data[], int y_data[]){
-		// Create Chart
 		type = "Linear";
 		Plot.plotChart(title, legenda, x_data, y_data);
 		
 	}
 	
 	public static void plotLinearChart(String title, String legenda, int y_data[]){
-		// Create Chart
 		type = "Linear";
 		Plot.plotChart(title, legenda, y_data);
 	}
 	
+	// 				GRAFICOS DE DISPERSAO
+	
 	public static void plotScatterChart(String title, String legenda, double x_data[], double y_data[]) {
-		// Create Chart
 		type = "Scatter";
 		Plot.plotChart(title, legenda, x_data, y_data);
 	}
 	
 	public static void plotScatterChart(String title, String legenda, double y_data[]) {
 		type = "Scatter";
-		// Show it
 		Plot.plotChart(title, legenda, y_data);
 	}
 	
 	public static void plotScatterChart(String title, String legenda, int x_data[], int y_data[]){
-		// Create Chart
 		type = "Scatter";
 		Plot.plotChart(title, legenda, x_data, y_data);
 		
@@ -131,20 +144,20 @@ public class Plot {
 		Plot.plotChart(title, legenda, y_data);
 	}
 	
+	
+	//			GRAFICOS DE AREA
+	
 	public static void plotAreaChart(String title, String legenda, double x_data[], double y_data[]) {
-		// Create Chart
 		type = "Area";
 		Plot.plotChart(title, legenda, x_data, y_data);
 	}
 	
 	public static void plotAreaChart(String title, String legenda, double y_data[]) {
 		type = "Area";
-		// Show it
 		Plot.plotChart(title, legenda, y_data);
 	}
 	
 	public static void plotAreaChart(String title, String legenda, int x_data[], int y_data[]){
-		// Create Chart
 		type = "Area";
 		Plot.plotChart(title, legenda, x_data, y_data);
 		
